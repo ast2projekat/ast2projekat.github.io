1f48bf7b720fe4f50ffb9af0d5d1813a53cfc7f9
ast2projekat <62513197+ast2projekat@users.noreply.github.com>
2020-03-23 20:12:39 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,2 @@
+
+alert('Ćao, ovo je naš istraživački dnevnik. Ideja je da da on prati naš rad, kako bi bilo preglednije šta smo to uspeli da uradimo. Takođe vaš prvi izaziv se nalazi pod brojem 3!');
