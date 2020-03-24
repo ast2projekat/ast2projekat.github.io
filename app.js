8659a95a28e17bf621c0b3a0a662eab298523246
ast2projekat <62513197+ast2projekat@users.noreply.github.com>
2020-03-24 17:33:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
 
-alert('Ćao, ovo je naš istraživački dnevnik. Ideja je da da on prati naš rad, kako bi bilo preglednije šta smo to uspeli da uradimo. Takođe vaš prvi izaziv se nalazi pod brojem 3!');
+alert('Ćao, ovo je naš istraživački dnevnik. Ideja je da da on prati naš rad, kako bi bilo preglednije šta smo to uspeli da uradimo. Takođe vaš prvi izazov se nalazi pod brojem 3!');
